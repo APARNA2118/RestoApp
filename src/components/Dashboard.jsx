86282904e31e7eb280e88d1e7703f779cc147262
APARNA T S <tsaparna@gmail.com>
2024-04-04 18:53:24 +0530

    user dash

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,6 +7,11 @@ import { Link } from 'react-router-dom';
 // import Feedback from './components/Feedback';
 // import AppCart from './AppCart';
 // import Order from './components/Order';
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
 
 
 
@@ -25,7 +30,7 @@ const images = [
   {
     url: 'assets/menu.png',
     title: 'MENU',
-    link: 'http://localhost:3000/menu',
+    link: '/menu',
     width: '30%',
   },
   {
@@ -104,9 +109,10 @@ const ImageMarked = styled('span')(({ theme }) => ({
 const Dashboard = () =>  {
   
       return (
+        <div>
         <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
           {images.map((image) => (
-            // <Link to={images.link} keys={images}>
+            //<Link to={images.link} keys={images}>
             <ImageButton
             //Link to={images.link} keys={images.title}
               focusRipple
@@ -136,9 +142,100 @@ const Dashboard = () =>  {
                 </Typography>
               </Image>
             </ImageButton>
-            // </Link>
+            
+            //</Link>
           ))}
+          
         </Box>
+        <br /><br />
+
+        <div className="orderpg" >
+      <Typography gutterBottom variant="h4" style={{fontWeight:'bold', fontFamily:'Montserrat', fontWeight:'700' , textAlign:'center'}}> Today's Offers</Typography>
+      <Grid container  style={{justify:'center' , alignItems:'center', direction:'column'}} >
+        <Box   height='200px' style={{display: '-webkit-inline-box' }}>
+           <Card style={{marginRight:'20px'}}>
+            <CardMedia
+             component='img'
+             height='200'
+             width='220'
+              image="https://www.bdtask.com/blog/assets/plugins/ckfinder/core/connector/php/uploads/images/promote-your-food-combo-offers.jpg"
+            
+            />
+             {/* <CardContent>
+               <Typography gutterBottom variant="h5" style={{fontWeight:'bold', fontFamily:'sans-serif'}}>
+               Bhatura
+               </Typography>
+               <Typography variant="body2" style={{ justifyContent:'center' , color:'black',fontFamily:'Montserrat',fontWeight:'400', fontSize:'16px'}}>
+               Bhatoora is a fluffy deep-fried leavened sourdough bread originating from the Indian subcontinent. It is commonly served as a midday meal or a breakfast dish in northern and eastern India.
+               </Typography>
+             </CardContent> */}
+           </Card>
+          
+           <Card style={{marginRight:'20px' }}>
+            <CardMedia
+             component='img'
+             height='200'
+             width='220'
+              image="https://www.bdtask.com/blog/assets/plugins/ckfinder/core/connector/php/uploads/images/categorize-your-food-combo.jpg"
+            
+            />
+             {/* <CardContent>
+               <Typography gutterBottom variant="h5" style={{fontWeight:'bold', fontFamily:'sans-serif'}}>
+               Chapati
+               </Typography>
+               <Typography variant="body2" style={{ justifyContent:'center' , color:'black',fontFamily:'Montserrat',fontWeight:'400', fontSize:'16px'}}>
+               Chapati, also known as roti, rooti, rotee rotli, rotta, safati, shabaati, phulka, chapo, sada roti, poli, and roshi, is an unleavened flatbread originating from the Indian subcontinent and is a staple in India.
+               </Typography>
+             </CardContent>     */}
+           </Card>
+
+
+           <Card style={{marginRight:'20px'}}>
+            <CardMedia
+             component='img'
+             height='200'
+             width='220'
+              image="https://www.dealnloot.com/wp-content/uploads/2019/03/faasos-bowl.png"
+            
+            />
+             {/* <CardContent>
+               <Typography gutterBottom variant="h5" style={{fontWeight:'bold', fontFamily:'sans-serif'}}>
+               Dosa
+               </Typography>
+               <Typography variant="body2" style={{ justifyContent:'center' , color:'black',fontFamily:'Montserrat',fontWeight:'400', fontSize:'16px'}}>
+               A dosa is a thin, savoury crepe in South Indian cuisine made from a fermented batter of ground white gram and rice. Dosas are served hot, often with chutney and sambar. Dosas are popular in South Asia as well as around the world
+               </Typography>
+             </CardContent> */}
+           </Card>
+
+           <Card style={{marginRight:'20px'}}>
+            <CardMedia
+             component='img'
+             height='200'
+             width='220'
+              image="https://www.bdtask.com/blog/assets/plugins/ckfinder/core/connector/php/uploads/images/add-drinks-to-make-a-delightful-food-combo.jpg"
+            
+            />
+             {/* <CardContent>
+               <Typography gutterBottom variant="h5" style={{fontWeight:'bold', fontFamily:'sans-serif'}}>
+               Fried Chicken
+               </Typography>
+               <Typography variant="body2" style={{ justifyContent:'center' , color:'black',fontFamily:'Montserrat',fontWeight:'400', fontSize:'16px'}}>
+               After some trial and error, they decided the chicken should be soaked in buttermilk and coated once in the breading mixture, then fried in oil at 350 °F (177 °C) in a pressure fryer until golden brown.
+               </Typography>
+             </CardContent> */}
+           </Card>
+         </Box>
+        </Grid>
+        {/* <br /><br />
+        <div className="all">
+        <Button variant="contained"  href="http://localhost:3000/menu" style={{alignContent:'center'}}>ORDER NOW</Button>
+        </div> */}
+      </div>
+      
+
+
+</div>
       
       );
 }
